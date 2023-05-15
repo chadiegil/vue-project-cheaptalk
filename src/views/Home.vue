fb4d317399bf153be763364c6960a4e3f604993c
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-05-15 11:00:16 +0800

    add filter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,17 @@
 <script setup>
-import { ref, onMounted, reactive, computed } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { db } from "../firebase/init.js";
 import { collection, getDocs } from "firebase/firestore";
 import Spinner from "../components/Spinner.vue";
+import SearchInput from "../components/SeachInput.vue";
 
 const posts = ref([]);
 const isLoading = ref(true);
+const query = ref("");
 
-const state = reactive({
-  filterdPost: posts,
-  searchQuery: "",
+const filteredPosts = computed(() => {
+  return posts.value.filter((post) => post.title.includes(query.value));
 });
-
-// const filterdPostsComputed = computed(() => {
-//   if (state.searchQuery) {
-//     return state.filterdPost.filter(
-//       (item) =>
-//         typeof item.text === "string" &&
-//         item.toLowerCase().includes(state.searchQuery.toLocaleLowerCase())
-//     );
-//   } else {
-//     return state.filterdPost;
-//   }
-// });
 
 onMounted(async () => {
   const querySnapshot = await getDocs(collection(db, "posts"));
@@ -43,11 +32,33 @@ onMounted(async () => {
 </script>
 
 <template>
-  <input v-model="state.searchQuery" type="text" placeholder="Search..." />
+  <!-- <SearchInput /> -->
+  <div class="search">
+    <input
+      type="text"
+      class="search__input"
+      v-model="query"
+      placeholder="Type your text"
+    />
+    <button class="search__button">
+      <svg class="search__icon" aria-hidden="true" viewBox="0 0 24 24">
+        <g>
+          <path
+            d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
+          ></path>
+        </g>
+      </svg>
+    </button>
+  </div>
   <div v-if="isLoading" class="center-spinner">
     <Spinner />
   </div>
-  <div v-else v-for="post in posts" :key="post.id" class="card-container">
+  <div
+    v-else
+    v-for="post in filteredPosts"
+    :key="post.id"
+    class="card-container"
+  >
     <div class="card">
       <h3 class="card__title">{{ post.title }}</h3>
       <p class="card__content">
@@ -145,5 +156,65 @@ onMounted(async () => {
 
 .card:hover .card__arrow svg {
   transform: translateX(3px);
+}
+
+/* input search */
+.search {
+  margin: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.search__input {
+  font-family: inherit;
+  font-size: inherit;
+  background-color: #f4f2f2;
+  border: none;
+  color: #646464;
+  padding: 0.7rem 1rem;
+  border-radius: 30px;
+  width: 12em;
+  transition: all ease-in-out 0.5s;
+  margin-right: -2rem;
+}
+
+.search__input:hover,
+.search__input:focus {
+  box-shadow: 0 0 1em #00000013;
+}
+
+.search__input:focus {
+  outline: none;
+  background-color: #f0eeee;
+}
+
+.search__input::-webkit-input-placeholder {
+  font-weight: 100;
+  color: #ccc;
+}
+
+.search__input:focus + .search__button {
+  background-color: #f0eeee;
+}
+
+.search__button {
+  border: none;
+  background-color: #f4f2f2;
+  margin-top: 0.1em;
+}
+
+.search__button:hover {
+  cursor: pointer;
+}
+
+.search__icon {
+  height: 1.3em;
+  width: 1.3em;
+  fill: #b4b4b4;
+}
+.search input:focus {
+  width: 250px;
 }
 </style>
